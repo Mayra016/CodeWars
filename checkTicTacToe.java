dc3f80e9179e5935fa42fec2cffb0c7a5a0303d1
Máyra <mayrazucaro@gmail.com>
2024-05-22 14:39:03 -0300

    Update checkTicTacToe.java

--- checkTicTacToe.java
+++ checkTicTacToe.java
@@ -4,7 +4,8 @@ public class CheckTicTacToe {
         int x = 0;
         int o = 0;
         int empty = 0;
-          
+      
+      
         if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
 
             if (board[0][0] == 1 || board[0][0] == 2) {
@@ -39,13 +40,17 @@ public class CheckTicTacToe {
             if (board[0][i] == 0) {
                 empty++;
             }
-            if (board[i][0] == 0) {
+            if (board[1][i] == 0) {
                 empty++;
-            }            
+            }
+            if (board[2][i] == 0) {
+                empty++;
+            }
           }
         }
-       
+
         return empty == 0 ? 0 : -1;
     }
 
 }
+
