6960db4202be54cfb12c691a2a6bd31547730b2b
Máyra <mayrazucaro@gmail.com>
2024-08-16 10:17:55 -0300

    Update makeUpperCase.cpp

--- makeUpperCase.cpp
+++ makeUpperCase.cpp
@@ -1,12 +1,11 @@
-#include <cctype>     
-#include <string>  
+#include <iostream>
+#include <string>
 #include <algorithm>
+#include <cctype>
 
 std::string makeUpperCase (const std::string& input_str)
 {
     std::string result = input_str;
-    std::transform(result.begin(), result.end(), result.begin(), [](unsigned char c) {
-        return std::toupper(c);
-    });
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
     return result;
 }
