19dd3b167bd46074819b967f3f9d95fcdbd6a5ce
Máyra <mayrazucaro@gmail.com>
2024-10-29 15:01:59 -0300

    Create Pofi.java

---
+++ Pofi.java
@@ -0,0 +1,22 @@
+public class Pofi {
+
+  public static String pofi(int n) {
+    if (n % 4 == 0) {
+      return "1";
+    }
+    
+    if (n % 4 == 1) {
+      return "i";
+    }
+    
+    if (n % 4 == 2) {
+      return "-1";
+    }
+    
+    if (n % 4 == 3) {
+      return "-i";
+    }
+    return "";
+  }
+    
+}
