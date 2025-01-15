cd2d0e36a6a8e273e867d3cc1ee40221420e6674
Máyra <mayrazucaro@gmail.com>
2025-01-15 17:02:48 -0300

    Create MaxMultiple.java

---
+++ MaxMultiple.java
@@ -0,0 +1,11 @@
+public class MaxMultiple {
+  public static int maxMultiple(int divisor, int bound) {
+    for (int i = bound; i > 0; i--) {
+      if (i % divisor == 0) {
+        return i;
+      }
+    }
+    
+    return 0;
+  }
+}
