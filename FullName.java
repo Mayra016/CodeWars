c2d3fbc5328983ed115c9ba67b5d50262d290bf1
Máyra <mayrazucaro@gmail.com>
2025-01-29 09:37:23 -0300

    Update FullName.java

--- FullName.java
+++ FullName.java
@@ -1,14 +1,26 @@
-public class FullName {
+public class Dinglemouse {
 
   private String firstName;
   private String lastName;
   
   public Dinglemouse(String firstName, String lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+    this.firstName = firstName.trim();
+    this.lastName = lastName.trim();
   }
   
   public String getFullName() { 
+    if (this.firstName == " " || this.firstName.isEmpty()) {
+      return this.lastName;
+    }
+    
+    if (this.lastName == " " || this.lastName.isEmpty()) {
+      return this.firstName;
+    }
+    
+    if (this.lastName == " " && this.firstName == " " || this.firstName.isEmpty() && this.lastName.isEmpty()) {
+      return " ";
+    }
+    
     return this.firstName + " " + this.lastName;
   }
   
