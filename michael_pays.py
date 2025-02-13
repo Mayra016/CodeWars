05931bb61a7b2c8c2e88b5570783c858f6f563a3
Máyra <mayrazucaro@gmail.com>
2025-02-13 16:50:57 -0300

    Create michael_pays.py

---
+++ michael_pays.py
@@ -0,0 +1,9 @@
+
+def michael_pays(cost):
+    if cost < 5:
+        return round(cost, 2)
+    
+    if cost / 3 <= 10:
+        return round(cost - cost / 3, 2)
+    else:
+        return round(cost - 10, 2)
