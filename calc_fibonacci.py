6604a90bf9f39d08e5e6e5496583a75bc6f890b5
Máyra <mayrazucaro@gmail.com>
2025-05-02 17:16:28 -0300

    Create calc_fibonacci.py

---
+++ calc_fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci():
+  x = 4
+  num = 1
+  result = []
+  
+  if x <= 2:
+      return [1, 1] if x == 2 else [1]
+ 
+  result = [1, 1]
+  
+  for i in range(2, x):
+      result.append(result[i - 2] + result[i - 1])
+  return result
