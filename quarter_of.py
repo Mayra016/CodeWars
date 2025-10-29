27ec6c3b4e83c0ca93f1fb796c15b3795471368d
Máyra <mayrazucaro@gmail.com>
2025-10-29 19:28:34 -0300

    Create quarter_of.py

---
+++ quarter_of.py
@@ -0,0 +1,9 @@
+def quarter_of(month):
+    if month >= 1 and month <= 3:
+        return 1
+    if month > 3 and month <= 6:
+        return 2
+    if month > 6 and month <= 9:
+        return 3
+    if month > 9 and month <= 12:
+        return 4
